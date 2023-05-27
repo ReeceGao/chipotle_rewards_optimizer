e6dd49ac0075ed62df345bb6d36826c6c2a5075d
Reece Gao <rgao1998@gmail.com>
2023-05-26 22:59:31 -0700

    response start page no header

--- client/src/components/StartPage/StartPage.js
+++ client/src/components/StartPage/StartPage.js
@@ -3,26 +3,34 @@ export default function StartPage({ onTypeHandler, onKeyDownHandler }) {
     return (
         <div className="container chipotle-brown">
             <div className="content">
-                <img
-                    src="../images/chip.png"
-                    alt="Chipotle Rewards Optimizer"
-                    className="rewards-icon"
-                />
-                <h1 className="purpose-text">
-                    Find out the best way to use your Chipotle Rewards points at
-                    local Chipotle resturants!
-                </h1>
-                <div className="input-section">
-                    <p className="enter-zipcode-text">Enter Zip Code:</p>
-                    <input
-                        className="input"
-                        type="text"
-                        minLength="5"
-                        maxLength="5"
-                        onChange={onTypeHandler}
-                        onKeyDown={onKeyDownHandler}
+                <div className="rewards-icon default-padding">
+                    <img
+                        src="../images/chip.png"
+                        alt="Chipotle Rewards Optimizer"
                     />
-                    <button className="search">Search</button>
+                </div>
+                <div className="purpose-container">
+                    <h1 className="purpose default-padding">
+                        Find out the best way to use your Chipotle Rewards
+                        points at local Chipotle resturants!
+                    </h1>
+                </div>
+                <div className="input-section default-padding">
+                    <p className="enter-zipcode-text">Enter Zipcode</p>
+                    <div>
+                        <input
+                            placeholder="e.g. 73072"
+                            className="input"
+                            type="text"
+                            minLength="5"
+                            maxLength="5"
+                            onChange={onTypeHandler}
+                            onKeyDown={onKeyDownHandler}
+                        />
+                        <span>
+                            <button className="search">Search</button>
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>

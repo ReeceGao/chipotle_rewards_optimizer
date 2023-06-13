d6ff7ff046066278ebcaae453bc4ddf30995e4ec
Reece Gao <rgao1998@gmail.com>
2023-06-12 17:55:04 -0700

    test change to server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const app = express();
 app.use(express.json()); //Needed this line otherwise there would be no req.body
 const port = 3113;
 
+app.get("/", (req, res) => {
+    res.send("hello world!");
+});
+
 app.post("/restaurants", (req, res) => {
     const { spawn } = require("child_process");
     const resturants = spawn("node", [
@@ -22,10 +26,28 @@ app.post("/prices", (req, res) => {
     prices.stdout.pipe(res);
 });
 
-app.get("/rewards", (req, res) => {
+app.get("/rewards", async (req, res) => {
     const { spawn } = require("child_process");
     const rewards = spawn("node", ["./scripts/getRewards.js"]);
-    rewards.stdout.pipe(res);
+    let data = "";
+    for await (const chunk of rewards.stdout) {
+        data += chunk;
+    }
+    let error = "";
+    for await (const chunk of rewards.stderr) {
+        error += chunk;
+    }
+    const exitCode = await new Promise((resolve, reject) => {
+        rewards.on("close", resolve);
+    });
+    if (exitCode) {
+        throw new Error(`subprocess error exit ${exitCode}, ${error}`);
+    }
+    res.send(JSON.parse(data));
+    // rewards.stdout.on("data", (data) => {
+    //     res.send(data.toString());
+    // });
+    // rewards.stdout.pipe(res);
 });
 
 app.listen(port, () => {

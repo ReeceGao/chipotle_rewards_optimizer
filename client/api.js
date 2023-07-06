783a6b8e60d469133d63dc60e34ca88c7a7124dc
Reece Gao <rgao1998@gmail.com>
2023-07-06 16:08:03 -0700

    testing removing the scripts

--- client/api.js
+++ client/api.js
@@ -1,6 +1,7 @@
 import express, { Router } from "express";
 import cors from "cors";
 import serverless from "serverless-http";
+import fetch from "node-fetch";
 
 const app = express();
 const router = Router();
@@ -64,22 +65,38 @@ router.post("/prices", async (req, res) => {
 });
 
 router.get("/rewards", async (req, res) => {
-    const { spawn } = await import("child_process");
-    const rewards = spawn("node", ["./scripts/getRewards.js"]);
-    let data = "";
-    for await (const chunk of rewards.stdout) {
-        data += chunk;
-    }
-    let error = "";
-    for await (const chunk of rewards.stderr) {
-        error += chunk;
-    }
-    const exitCode = await new Promise((resolve, reject) => {
-        rewards.on("close", resolve);
+    // const { spawn } = await import("child_process");
+    // const rewards = spawn("node", ["./scripts/getRewards.js"]);
+    const rewardsURL =
+        "https://services.chipotle.com/rewardstore/v1/rewardstore/web";
+
+    const rewardsHeader = new Headers({
+        "Content-Type": "application/json",
+        "Ocp-Apim-Subscription-Key": "b4d9f36380184a3788857063bce25d6a",
+        Authorization:
+            "Bearer eyJhbGciOiJBMTI4S1ciLCJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwidHlwIjoiSldUIn0.DJA8aPuoevEbNdBr3alC719HEjkJ6AEYr3SaaajzcJsV7pdDRTpROg.pf61J7-XwBtKzkWCQ5Jc2w.MZrhiYbw_j1n1AFEksKow_2LKa-5aDH-902Tf1KjOHP_tWZB-P27ezBeMb6gkTsC685-D7nDrTEsh5LaxhV0suvekqNCpfOsMmUGOY2vLgj7e7WYyG8-pNZKbH8oNz2zSrXj-qForFUNGqIBbJbuD9DP4rcaqr3-9bP2NHLj9B0Gyqf0eR7mqHUH8An96AhufC_iKtO_8-9L7EZ8luwkxAMowMDEU94jz50-mPM0MKK8mgk9afJhYz7JLNVeJw7XDng2iejY_-WuJgmZI6cuDTLwU7DEHcqrePttWcHKUJGzM3KhigpNruckwaZ-nJWRSq6PKI8XJW1RvjT-KuW3dvucXDhRoYFuL0n_OKcoR7C87KNi-_MWis_2-sj2NUh8eusTREDKVaM804r_N7OgaS6ppjtauWg-XuQ4W4zdi8Qj2xu0N8inJ0E7s5Q8pvjxIjkdL_cTFK6ryFbZVM6t5F5jnDkOZmbLMmvbgjn7P8lVxf2jIepjAtIK1rSlKZCh.mmztN6B3p5tG6ZDwk1yVlQ",
     });
-    if (exitCode) {
-        throw new Error(`subprocess error exit ${exitCode}, ${error}`);
-    }
+
+    const rewardsOptions = {
+        method: "GET",
+        headers: rewardsHeader,
+    };
+    const res = await fetch(rewardsURL, rewardsOptions);
+    const data = res.json();
+    // let data = "";
+    // for await (const chunk of rewards.stdout) {
+    //     data += chunk;
+    // }
+    // let error = "";
+    // for await (const chunk of rewards.stderr) {
+    //     error += chunk;
+    // }
+    // const exitCode = await new Promise((resolve, reject) => {
+    //     rewards.on("close", resolve);
+    // });
+    // if (exitCode) {
+    //     throw new Error(`subprocess error exit ${exitCode}, ${error}`);
+    // }
     res.send(JSON.parse(data));
 });
 

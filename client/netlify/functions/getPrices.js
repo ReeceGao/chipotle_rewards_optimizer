313caec8a327ebbffdb30f3b64f4bea79b4c8a8e
Reece Gao <rgao1998@gmail.com>
2023-07-07 00:59:47 -0700

    testing get prices and restaurants

---
+++ client/netlify/functions/getPrices.js
@@ -0,0 +1,22 @@
+exports.handler = async function (event, context) {
+    const restaurantId = event.body.restaurantId;
+    const pricesUrl = `https://services.chipotle.com/menuinnovation/v1/restaurants/${restaurantId}/onlinemenu?channelId=web&includeUnavailableItems=true`;
+
+    const getPricesHeader = new Headers({
+        "Content-Type": "application/json",
+        "Ocp-Apim-Subscription-Key": "b4d9f36380184a3788857063bce25d6a",
+    });
+
+    const pricesOptions = {
+        method: "GET",
+        headers: getPricesHeader,
+    };
+
+    const response = await fetch(pricesUrl, pricesOptions);
+    const prices = await response.json();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(prices),
+    };
+};
---
+++ client/netlify/functions/getRestaurants.js
@@ -0,0 +1,47 @@
+exports.handler = async function (event, context) {
+    const restaurantsUrl =
+        "https://services.chipotle.com/restaurant/v3/restaurant";
+
+    const getRestaurantHeaders = new Headers({
+        "Content-Type": "application/json",
+        "Ocp-Apim-Subscription-Key": "b4d9f36380184a3788857063bce25d6a",
+    });
+
+    const payload = {
+        latitude: parseFloat(event.body.latLng.lat),
+        longitude: parseFloat(event.body.latLng.lng),
+        radius: 80467,
+        restaurantStatuses: ["OPEN", "LAB"],
+        conceptIds: ["CMG"],
+        orderBy: "distance",
+        orderByDescending: false,
+        pageSize: 10,
+        pageIndex: 0,
+        embeds: {
+            addressTypes: ["MAIN"],
+            realHours: true,
+            directions: true,
+            catering: true,
+            onlineOrdering: true,
+            timezone: true,
+            marketing: true,
+            chipotlane: true,
+            sustainability: true,
+            experience: true,
+        },
+    };
+
+    const resOptions = {
+        method: "POST",
+        headers: getRestaurantHeaders,
+        body: JSON.stringify(payload),
+    };
+
+    const response = await fetch(restaurantsUrl, resOptions);
+    const restaurants = await response.json();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(restaurants),
+    };
+};

d9798cba543ef60358b2c9ad1efd264a2ba2ef3b
Reece Gao <rgao1998@gmail.com>
2023-08-15 19:13:12 -0700

    adding purpose text to header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,6 +5,10 @@ export default function Header() {
             <button href="#" className="site-logo">
                 <img src="./images/logo.png" alt="site logo" />
             </button>
+            <div className="fw-600 purpose-text">
+                Find out which Chipotle rewards are most worth it at your local
+                Chipotle restaurants!
+            </div>
         </div>
     );
 }
